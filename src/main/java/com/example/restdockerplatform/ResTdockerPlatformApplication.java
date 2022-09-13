47c6e43e1ac69ad4f2743451041426378be8b1ff
Jacek Kitlinski <gasniczka@o2.pl>
2022-09-13 14:39:28 +0200

    inital application setup

---
+++ src/main/java/com/example/restdockerplatform/ResTdockerPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.restdockerplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTdockerPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTdockerPlatformApplication.class, args);
+    }
+
+}
