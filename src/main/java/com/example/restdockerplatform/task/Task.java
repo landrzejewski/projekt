587f21a0ecc04aecab0cc08530aa9ec5fb372e2f
JoannaMorgas <asia.morgas@gmail.com>
2022-09-21 00:56:43 +0200

    initial db config

---
+++ src/main/java/com/example/restdockerplatform/task/Task.java
@@ -0,0 +1,32 @@
+package com.example.restdockerplatform.task;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int Id;
+
+    private String userName;
+    private String project;
+    private TaskStatus status;
+    private String result;
+
+    public Task(String userName, String project, TaskStatus status, String result) {
+        this.userName = userName;
+        this.project = project;
+        this.status = status;
+        this.result = result;
+    }
+}
