ff441363ac6aa1b7deeb3bc7afbe3f6127a53d1d
Piotr Baranowski <baranosiu@gmail.com>
2022-10-19 20:51:52 +0200

    Refaktoryzacja testu (aby poszczególne testy nie używały wspólnego obiektu klasy Task)

--- agent/src/test/java/local/wspolnyprojekt/nodeagent/task/TaskTest.java
+++ agent/src/test/java/local/wspolnyprojekt/nodeagent/task/TaskTest.java
@@ -7,26 +7,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class TaskTest {
 
-    private Task task;
-
     @BeforeEach
     void init() {
-        task = new Task(null,null,null);
     }
 
     @Test
     void shouldGiveTrueWhenSendingNextStatusIsDisabled() {
+        Task task;
+        task = new Task(null,null,null);
         task.disableSendingNextStatus();
         assertThat(task.getAndResetSendingNextStatus()).isTrue();
     }
 
     @Test
     void shouldGiveFalseWhenSendingNextStatusIsDefault() {
+        Task task;
+        task = new Task(null,null,null);
         assertThat(task.getAndResetSendingNextStatus()).isFalse();
     }
 
     @Test
     void shouldResetToDefaultAfterCallingGetAndResetSendingNextStatusMethod() {
+        Task task;
+        task = new Task(null,null,null);
         task.disableSendingNextStatus();
         assertThat(task.getAndResetSendingNextStatus()).isTrue(); // Pierwsze wywołanie - enabled
         assertThat(task.getAndResetSendingNextStatus()).isFalse(); // Drugie wywołanie - disabled
