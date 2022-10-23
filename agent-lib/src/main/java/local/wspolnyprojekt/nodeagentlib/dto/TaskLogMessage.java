a3f594bfc497f52d944f3c9697513a017b55de8c
Paweł Soćko <sokerowsky@gmail.com>
2022-10-23 21:10:13 +0200

    Added JsonCreator annotation

--- agent-lib/src/main/java/local/wspolnyprojekt/nodeagentlib/dto/TaskLogMessage.java
+++ agent-lib/src/main/java/local/wspolnyprojekt/nodeagentlib/dto/TaskLogMessage.java
@@ -1,5 +1,7 @@
 package local.wspolnyprojekt.nodeagentlib.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 @Value
@@ -7,4 +9,13 @@ public class TaskLogMessage implements JsonString {
     String timestamp;
     String senderId;
     String payload;
+
+    @JsonCreator
+    public TaskLogMessage(@JsonProperty("timestamp") String timestamp,
+                          @JsonProperty("senderId") String senderId,
+                          @JsonProperty("payload") String payload) {
+        this.timestamp = timestamp;
+        this.senderId = senderId;
+        this.payload = payload;
+    }
 }
