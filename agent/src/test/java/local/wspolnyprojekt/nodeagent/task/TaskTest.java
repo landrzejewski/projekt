6dda7de4a2466734d34b46aeddc9d68e611b7a57
Piotr Baranowski <baranosiu@gmail.com>
2022-11-30 21:06:03 +0100

    Uzupełnienie testów klasy Task i sprawdzenie czy idzie broadcast zmiany stanu zadania.

--- agent/src/test/java/local/wspolnyprojekt/nodeagent/task/TaskTest.java
+++ agent/src/test/java/local/wspolnyprojekt/nodeagent/task/TaskTest.java
@@ -1,9 +1,16 @@
 package local.wspolnyprojekt.nodeagent.task;
 
+import local.wspolnyprojekt.nodeagent.statusbroadcast.StatusBroadcaster;
+import local.wspolnyprojekt.nodeagent.task.state.TaskState;
+import local.wspolnyprojekt.nodeagent.task.state.TaskStateFail;
+import local.wspolnyprojekt.nodeagent.task.state.TaskStateReady;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
 
 class TaskTest {
 
@@ -14,7 +21,7 @@ class TaskTest {
     @Test
     void shouldGiveTrueWhenSendingNextStatusIsDisabled() {
         Task task;
-        task = new Task(null,null,null);
+        task = new Task(null, null, null);
         task.disableSendingNextStatus();
         assertThat(task.getAndResetSendingNextStatus()).isFalse();
     }
@@ -22,16 +29,35 @@ class TaskTest {
     @Test
     void shouldGiveFalseWhenSendingNextStatusIsDefault() {
         Task task;
-        task = new Task(null,null,null);
+        task = new Task(null, null, null);
         assertThat(task.getAndResetSendingNextStatus()).isTrue();
     }
 
     @Test
     void shouldResetToDefaultAfterCallingGetAndResetSendingNextStatusMethod() {
         Task task;
-        task = new Task(null,null,null);
+        task = new Task(null, null, null);
         task.disableSendingNextStatus();
         assertThat(task.getAndResetSendingNextStatus()).isFalse(); // Pierwsze wywołanie - disabled
         assertThat(task.getAndResetSendingNextStatus()).isTrue(); // Drugie wywołanie - enabled
+    }
+
+    @Test
+    void shouldSetState() {
+        StatusBroadcaster statusBroadcaster = Mockito.mock(StatusBroadcaster.class);
+        Task task = new Task(statusBroadcaster, null, null);
+        TaskState stateReady = new TaskStateReady();
+        task.setStatus(stateReady);
+        assertThat(task.getStatus()).isEqualTo(stateReady);
+    }
+
+    @Test
+    void shouldBroadcastState() {
+        StatusBroadcaster statusBroadcaster = Mockito.mock(StatusBroadcaster.class);
+        Task task = new Task(statusBroadcaster, null, null);
+        task.setStatus(new TaskStateReady());
+        task.setStatus(new TaskStateFail(),"FAIL");
+        Mockito.verify(statusBroadcaster,times(1)).broadcastStatusChange(task,"");
+        Mockito.verify(statusBroadcaster,times(1)).broadcastStatusChange(task,"FAIL");
     }
 }
